15c4ccffa796115935b7ad80cd6683c4d0f26fa1
Charles Davidson <cddavidson86@yahoo.com>
2019-06-10 17:29:04 -0500

    adding final project after internet was out

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-
+This is a giggy grabber. It takes the search word you entered and searches for the top giffys of that topic and returns them in an arranged format. You can animate them by clicking on the image. 
--- assets/javascript/giffy.js
+++ assets/javascript/giffy.js
@@ -1,44 +1,115 @@
- // Initial array of movies
- var movies = ["The Matrix", "The Notebook", "Mr. Nobody", "The Lion King"];
+$(document).ready(() => {
+  // GLOBALS
+  var gifAPI = "https://api.giphy.com/v1/gifs/";
+  var gifDevKey = "ddorAj1BeWlKJQEUjbRpNklvrn8YJGZV";
+  var topics = ["Cats", "Dogs", "Fish", "Ducks"];
 
- // Function for displaying movie data
- function renderButtons() {
+  (function onFirstLoad() {
+    getGiphy("search", topics[0]);
 
-   // Deleting the movie buttons prior to adding new movie buttons
-   // (this is necessary otherwise we will have repeat buttons)
-   $("#buttons-view").empty();
+    renderButtons();
+  })();
 
-   // Looping through the array of movies
-   for (var i = 0; i < movies.length; i++) {
+  //// EVENT LISTENERS
 
-     // Then dynamicaly generating buttons for each movie in the array.
-     // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-     var a = $("<button>");
-     // Adding a class
-     a.addClass("movie");
-     // Adding a data-attribute with a value of the movie at index i
-     a.attr("data-name", movies[i]);
-     // Providing the button's text with a value of the movie at index i
-     a.text(movies[i]);
-     // Adding the button to the HTML
-     $("#buttons-view").append(a);
-   }
- }
+  // Event on any gif button
+  $(".button-box").on("click", ".btn-default", queryAPI);
 
- // This function handles events where one button is clicked
- $("#add-movie").on("click", function(event) {
-   // event.preventDefault() prevents the form from trying to submit itself.
-   // We're using a form so that the user can hit enter instead of clicking the button if they want
-   event.preventDefault();
+  // Event to [de]active the gif
+  $(".gifs-container").on("click", ".gif-frame img", function() {
+    var active = $(this).attr("data-active") || "false";
 
-   // This line will grab the text from the input box
-   var movie = $("#movie-input").val().trim();
-   // The movie from the textbox is then added to our array
-   movies.push(movie);
+    if (active === "false") {
+      $(this).attr("data-active", "true");
+      $(this).attr("src", $(this).data("moving"));
+    } else {
+      $(this).attr("data-active", "false");
+      $(this).attr("src", $(this).data("still"));
+    }
 
-   // calling renderButtons which handles the processing of our movie array
-   renderButtons();
- });
+    $(this).toggleClass("active-gif");
+  });
 
- // Calling the renderButtons function at least once to display the initial list of movies
- renderButtons();
+  // Event on Form Button
+  $("button#add-gif").on("click", e => {
+    e.preventDefault();
+    var gif = $("#gif-input")
+      .val()
+      .trim();
+
+    getGiphy("search", gif);
+
+    if (gif.length > 0 && topics.indexOf(gif) === -1) {
+        topics.push(gif);
+      renderButtons();
+    }
+    $("#gif-input").val("");
+  });
+
+  // Function to create buttons
+  function renderButtons() {
+    $(".button-box").empty();
+
+    for (var i in topics) {
+      var b = $("<button>");
+
+      b.addClass("btn btn-default");
+      // Added a data-attribute
+      b.attr("data-name", topics[i]);
+      // Provided the initial button text
+      b.text(topics[i]);
+      // Added the button to the buttons-view div
+      $(".button-box").append(b);
+    }
+  }
+
+  function queryAPI() {
+    //pull queryString from callers data-name attribute
+    console.log($(this).attr("data-name"));
+    getGiphy("search", $(this).attr("data-name"));
+  }
+
+  function getGiphy(queryType, queryStr) {
+    var queryURL =
+      gifAPI +
+      queryType +
+      "?q=" +
+      queryStr +
+      "&rating=pg&limit=10&api_key=" +
+      gifDevKey;
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(response => {
+      console.log("finished ajax");
+      var gifObjs = response.data,
+        gifCont = $(".gifs-container");
+
+      gifCont.empty();
+
+      if (gifObjs.length == 0) {
+        gifCont.append("<div>Sorry. Nothing exists. Try again!");
+        console.log(topics, topics.indexOf(queryStr));
+        topics.splice(topics.indexOf(queryStr), 1);
+        renderButtons();
+      } else {
+        gifObjs.forEach(gif => {
+          var gifFrame = $("<div>").addClass("gif-frame");
+          var gifImg = $("<img>").append(
+            "<span class='rating'>" + gif.rating + "</span>"
+          );
+
+          gifImg.data("still", gif.images.original_still.url);
+          // initial img is the still version
+          gifImg.attr("src", gif.images.original_still.url);
+          gifImg.data("moving", gif.images.original.url);
+          // Add img to Frame
+          gifFrame.append(gifImg);
+          // Add html to DOM
+          gifCont.append(gifFrame);
+        });
+      }
+    });
+  }
+});
